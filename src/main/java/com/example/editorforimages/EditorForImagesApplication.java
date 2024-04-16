5829a4822ae9d5f755a5db66bb5c82d9dab7ebb3
necuteboy <andreychikov32@mail.ru>
2024-04-16 22:43:58 +0500

    init commit

---
+++ src/main/java/com/example/editorforimages/EditorForImagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.editorforimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorForImagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditorForImagesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/editorforimages/EditorForImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.editorforimages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorForImagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
